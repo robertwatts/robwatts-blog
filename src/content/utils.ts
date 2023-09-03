7e25844b38016f5bac0711ed9644e662695827b5
Robert Watts <robw@wonderbly.com>
2023-09-03 08:47:02 +0100

    /posts route and css

--- src/content/utils.ts
+++ src/content/utils.ts
@@ -1,4 +1,17 @@
+import { CollectionEntry, getCollection } from 'astro:content';
 import type { ContentType } from "./types";
+
+/**
+ * Returns all entries in the blog content collection.
+ * Regex is required to filter README.md from the collection.
+ */
+export async function getBlogEntries(): Promise<CollectionEntry<"blog">[]> {
+  const blogDirectoryRegex = /^.+\/.+\..+$/;
+  const entries = await getCollection("blog", ({ id, data }) => {
+    return blogDirectoryRegex.test(id as string) && data.draft !== true;
+  });
+  return entries;
+}
 
 export function getContentTypeById(id: string): ContentType {
   const folder = id.split('/')[0];
